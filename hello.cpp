4ab014e03ba8b63ba3a4f3c1f8d5df1e039cec6d
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2021-06-06 01:45:45 -0500

    Add files via upload

--- hello.cpp
+++ hello.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
    // printf() displays the string inside quotation
    #ifdef __TINYC__
-    std::cout << "Hello, World!\nFrom TinyCC.\n";
+    std::printf("Hello, World!\nFrom TinyCC.\n");
    #endif
    #ifndef __TINYC__
     #ifdef __GNUC__
      #ifdef __clang__
       #ifdef __cplusplus
-       std::cout << "Hello, World!\nFrom Clang++.\n";
+       std::printf("Hello, World!\nFrom Clang++.\n");
       #endif
       #ifndef __cplusplus
-       std::cout << "Hello, World!\nFrom Clang.\n";
+       std::printf("Hello, World!\nFrom Clang.\n");
       #endif
      #endif
      #ifndef __clang__
       #ifdef __GNUG__
-       std::cout << "Hello, World!\nFrom G++.\n";
+       std::printf("Hello, World!\nFrom G++.\n");
       #endif
       #ifndef __GNUG__
        #ifdef __cplusplus
-        std::cout << "Hello, World!\nFrom G++.\n";
+        std::printf("Hello, World!\nFrom G++.\n");
        #endif
        #ifndef __cplusplus
-        std::cout << "Hello, World!\nFrom GCC.\n";
+        std::printf("Hello, World!\nFrom GCC.\n");
        #endif
       #endif
      #endif
@@ -33,18 +33,18 @@ int main(int argc, char *argv[])
     #ifndef __GNUC__
      #ifdef _MSC_VER
       #ifdef __cplusplus
-       std::cout << "Hello, World!\nFrom MSVC++.\n";
+       std::printf("Hello, World!\nFrom MSVC++.\n");
       #endif
       #ifndef __cplusplus
-       std::cout << "Hello, World!\nFrom MSVC.\n";
+       std::printf("Hello, World!\nFrom MSVC.\n");
       #endif
      #endif
      #ifndef _MSC_VER
       #ifdef __cplusplus
-       std::cout << "Hello, World!\nFrom Somewhere++.\n";
+       std::printf("Hello, World!\nFrom Somewhere++.\n");
       #endif
       #ifndef __cplusplus
-       std::cout << "Hello, World!\nFrom Somewhere.\n";
+       std::printf("Hello, World!\nFrom Somewhere.\n");
       #endif
      #endif
     #endif
